7fd5cf586e2db0f56ca7d0a43c7cacb946edd255
overfeld.philipp <poverfeld@deloitte.de>
2018-01-15 18:52:38 +0100

    table and charting

--- routes/backtesting.js
+++ routes/backtesting.js
@@ -18,6 +18,14 @@ router.get('/table', function(req, res, next) {
     //res.send("Historic Data loaded from file: " + backtestingTradeTable.data.intervalls);
 
     res.render('table', {TableName: backtestingTradeTable.name, TableRows: backtestingTradeTable.data.intervalls });
-  });
+});
+
+router.get('/charts', function(req, res, next) {
+    
+
+  //res.send("Historic Data loaded from file: " + backtestingTradeTable.data.intervalls);
+
+  res.render('charts', {});
+});
 
 module.exports = router;
